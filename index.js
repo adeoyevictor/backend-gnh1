8efe5a4e3dbc0860dd24eae1a512cecab8fb95a0
adeoyevictor <adeoyevictor16@gmail.com>
2023-09-07 13:57:30 +0100

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const express = require("express");
+const express = require("express")
 const app = express()
+const moment = require("moment")
 
 app.get('/api', (req, res) => {
+    const slack_name = req.query.slack_name
+    const track = req.query.track
+    
+    if(!slack_name || !track){
+        return res.status(400).json({
+            error: "please provide_slack name and track as GET params"
+        })
+    }
+    const current_day = moment().format("dddd")
+    const utc_now = moment.utc()
+    const utc_time = utc_now.format("YYYY-MM-DDTHH:mm:ss[Z]")
+
     res.json({
-      slack_name: "example_name",
-      current_day: "Monday",
-      utc_time: "2023-08-21T15:04:05Z",
-      track: "backend",
+      slack_name,
+      current_day,
+      utc_time,
+      track,
       github_file_url:
-        "https://github.com/username/repo/blob/main/file_name.ext",
-      github_repo_url: "https://github.com/username/repo",
+        "https://github.com/adeoyevictor/backend-gnh1/blob/main/index.js",
+      github_repo_url:
+        "https://github.com/adeoyevictor/backend-gnh1",
       status_code: 200,
     });
 })
 
 const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
